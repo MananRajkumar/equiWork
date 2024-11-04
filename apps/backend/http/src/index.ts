d87980dce503cc74614f7b7780515273ad536ced
“MananRajkumar” <mananrajkumar98@gmail.com>
2024-11-04 14:44:26 +0530

    Initial Commit

---
+++ apps/backend/http/src/index.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import { config } from "dotenv";
+
+config();
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+    console.log(`Server started listening on PORT ${PORT}`);
+});
